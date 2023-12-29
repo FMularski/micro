918c9472d52b7d33d2d627197c095a9a5b9749ed
Filip Mularski <mularskif@gmail.com>
2023-12-29 22:42:56 +0100

    [email] updated run_consumer command

--- email/core/management/commands/run_consumer.py
+++ email/core/management/commands/run_consumer.py
@@ -1,13 +1,24 @@
 from django.core.management.base import BaseCommand
 from email_service.consumer import Consumer
+from pika.exceptions import AMQPConnectionError
 
 
 class Command(BaseCommand):
     help = "Starts AMQP consumer."
 
+    def _run_consumer(self, consumer):
+        """
+        Try again if connection cannot be established yet.
+        """
+        try:
+            consumer.connect()
+        except AMQPConnectionError:
+            self._run_consumer(consumer)
+
+        consumer.consume()
+
     def handle(self, *args, **options):
         self.stdout.write("Starting AMQP consumer...", self.style.WARNING)
 
         consumer = Consumer()
-        consumer.connect()
-        consumer.consume()
+        self._run_consumer(consumer)
