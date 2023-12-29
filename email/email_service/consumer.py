e11dacf66e4556864e5080c4b60aad5c633b7ddd
Filip Mularski <mularskif@gmail.com>
2023-12-29 22:23:54 +0100

    [email] updated Consumer class

--- email/email_service/consumer.py
+++ email/email_service/consumer.py
@@ -3,18 +3,37 @@ from django.conf import settings
 
 
 class Consumer:
+    """
+    Class consuming messages from queues.
+    """
+
+    QUEUES_CALLBACKS = {
+        "user-created": "send_welcome_email",
+        # more queues
+        # ...
+    }
+
     def __init__(self):
         self._params = pika.URLParameters(settings.MQ_URL)
         self._conn = None
         self._channel = None
 
     def connect(self):
-        self._conn = pika.BlockingConnection(self._params)
-        self._channel = self._conn.channel()
+        if not self._conn or self._conn.is_closed:
+            self._conn = pika.BlockingConnection(self._params)
+            self._channel = self._conn.channel()
 
     def send_welcome_email(self, channel, method, properties, body):
         print("CONSUMER: sending email...")
 
+    def _bind_queues_to_callback(self):
+        for queue, callback in self.QUEUES_CALLBACKS.items():
+            self._channel.basic_consume(queue, getattr(self, callback), auto_ack=True)
+
     def consume(self):
-        self._channel.basic_consume("user-created", self.send_welcome_email, auto_ack=True)
+        self._bind_queues_to_callback()
         self._channel.start_consuming()
+
+    def close(self):
+        if self._conn and self._conn.is_open:
+            self._conn.close()
