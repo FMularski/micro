6c841c92df48d034e6328e8adf482b35f2cddcaf
Filip Mularski <mularskif@gmail.com>
2024-01-02 19:38:48 +0100

    updated users/Dockerfile and email/email_service/settings.py

--- email/email_service/settings.py
+++ email/email_service/settings.py
@@ -119,8 +119,8 @@ CELERY_BROKER_URL = MQ_URL
 
 # email
 EMAIL_BACKEND = env.str("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
-EMAIL_HOST = env.str("EMAIL_HOST")
-EMAIL_PORT = env.str("EMAIL_PORT")
-EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = env.str("EMAIL_HOST", "")
+EMAIL_PORT = env.str("EMAIL_PORT", "")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", "notifier@example.com")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
